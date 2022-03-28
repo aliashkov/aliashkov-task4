33fb6599949e8d12aacd125cd7d5542eae6c738f
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-03-28 11:51:16 +0300

    Update Task2.js

--- Task2.js
+++ Task2.js
@@ -1,1 +1,16 @@
-https://www.codewars.com/kata/541c8630095125aba6000c00/solutions/javascript
+function digital_root(n) {
+    let sum = 0;
+
+    if (n < 10)
+        return n;
+        
+    for (let i = 0; i < String(n).length; i++)
+        sum += Number(String(n)[i]);
+     
+    return digital_root(sum);
+}
+
+
+console.log(digital_root(16));
+console.log(digital_root(58));
+console.log(digital_root(123456));
