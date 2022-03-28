c2e6aa8f03b8b3e437a93655c3b1f1924efb3614
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-03-28 11:51:35 +0300

    Update Task3.js

--- Task3.js
+++ Task3.js
@@ -1,1 +1,13 @@
-https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/solutions/javascript
+function sortArray(array) {
+    let modifiedArray = array.filter(n => n % 2).sort((a, b) => a - b);
+
+    for (let j = 0; j < array.length; j++) {
+        if (array[j] % 2 === 0)
+            modifiedArray.splice(j, 0, array[j]);
+    }
+
+    return modifiedArray;
+}
+
+console.log(sortArray([5, 8, 6, 3, 4]));
+console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
