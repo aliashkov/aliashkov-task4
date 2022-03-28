3502884a663a3f4366ddf50b05c3f18d2b037d68
aliashkov <42827717+aliashkov@users.noreply.github.com>
2022-03-28 11:50:57 +0300

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -1,12 +1,16 @@
-const sum  = function (a) {
-    return function (b) {
-      if (b) {
-        return sum(a+b);
-      }
-      return a; 
-    }
-  };
-  
-  
-  console.log(sum(1)(2)()); //3
-  console.log(sum(1)(2)(3)(4)()); //10
+function add(...args) {
+  let a = args.reduce((a, b) => a + b, 0);
+
+  return function (...args) {
+    let b = args.reduce((a, b) => a + b, 0);
+
+    if (b)
+      return add(a + b);
+
+    return a
+  }
+}
+
+
+console.log(add(1)(4)(7)()); //3
+console.log(add(1)(2)(3)(4)()); //10
